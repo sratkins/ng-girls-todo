58e125d8af07bb72f2149150bde7fa56ffaa9395
Sarah Atkins <sratkins92@gmail.com>
2020-07-25 16:06:51 -0500

    Created list-manager component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,34 +2,18 @@ import { Component } from '@angular/core';
 import { TodoItem } from './interfaces/todo-item';
 
 @Component({
-  selector: 'app-root',
-  template: `
-    <h1>
-      Welcome to {{title}}!
-    </h1>
+ selector: 'app-root',
+template: `
+  <h1>
+    Welcome to {{ title }}!
+  </h1>
 
-    <app-input-button-unit (submit)="addItem($event)"></app-input-button-unit>
+  <app-list-manager></app-list-manager>
 
-    <ul>
-      <li *ngFor="let todoItem of todoList">
-        <app-todo-item [item]="todoItem"></app-todo-item>
-      </li>
-    </ul>
-  `,
-  styleUrls: ['./app.component.css']
+ `,
+ styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
-  todoList = [
-    {title: 'install NodeJS'},
-    {title: 'install Angular CLI'},
-    {title: 'create new app'},
-    {title: 'serve app'},
-    {title: 'develop app'},
-    {title: 'deploy app'},
-  ];
 
-  addItem(title: string){
-    this.todoList.push({ title });
-  }
+export class AppComponent {
+  title = 'My To Do List APP';
 }
