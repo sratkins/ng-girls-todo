30c960cc62d0a166585efa1848466b6aaa686f0e
Sarah Atkins <sratkins92@gmail.com>
2020-08-19 12:04:10 -0500

    Fixed todo-list service problem
    added the getTodoList function

--- src/app/services/todo-list.service.ts
+++ src/app/services/todo-list.service.ts
@@ -1,22 +1,41 @@
 import { Injectable } from '@angular/core';
-import { TodoItem } from '../interfaces/todo-item'
+import { TodoItem } from '../interfaces/todo-item';
+import { StorageService } from './storage.service';
+
+const todoListStorageKey = 'Todo_List';
+
+const defaultTodoList = [
+  {title: 'install NodeJS'},
+  {title: 'install Angular CLI'},
+  {title: 'create new app'},
+  {title: 'serve app'},
+  {title: 'develop app'},
+  {title: 'deploy app'},
+];
 
 @Injectable({
   providedIn: 'root'
 })
 export class TodoListService {
-  todoList = [
-    {title: 'install NodeJS'},
-    {title: 'install Angular CLI'},
-    {title: 'create new app'},
-    {title: 'serve app'},
-    {title: 'develop app'},
-    {title: 'deploy app'},
-  ];
+  todoList: TodoItem[];
 
-  constructor() { }
+  constructor(private storageService: StorageService) {
+    this.todoList =
+      storageService.getData(todoListStorageKey) || defaultTodoList;
+   }
 
-  getTodoList(){
-    return this.todoList;
-  }
+   addItem(item: TodoItem){
+     this.todoList.push(item);
+     this.storageService.setData(todoListStorageKey, this.todoList);
+   }
+
+   updateItem(item: TodoItem, changes){
+     const index = this.todoList.indexOf(item);
+     this.todoList[index] = { ... item, ...changes };
+     this.storageService.setData(todoListStorageKey, this.todoList);
+   }
+
+   getTodoList(){
+     return this.todoList;
+   }
 }
